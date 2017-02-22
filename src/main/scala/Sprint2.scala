9f71d314d55c7a3c38ffd324a68118bd09a07fe2
Pascal Bugnion <pascal@bugnion.org>
2017-02-22 06:47:16 +0000

    Working code for sprint 2

--- src/main/scala/Sprint2.scala
+++ src/main/scala/Sprint2.scala
@@ -1,47 +1,70 @@
 
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration._
+
+import java.nio.file.{Paths, StandardOpenOption}
+import java.time.LocalDateTime
 
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.NotUsed
+import akka.util.ByteString
 
+import play.api.libs.json.{JsObject, Json}
 import play.api.libs.ws.ahc.AhcWSClient
 
 object Sprint2 extends App {
 
   // Instantiate an actor system and materializer
-  implicit val system = ActorSystem("Sprint1")
+  implicit val system = ActorSystem("Sprint2")
   import system.dispatcher // provides an execution context
   implicit val materializer = ActorMaterializer()
 
   // We need a web service client for querying the API
   implicit val ws = AhcWSClient()
 
+  val outputPath = Paths.get("postcode_restaurants.json")
   val parallelismLevel = 8 // Number of concurrent threads to use to query the Yelp API
+
+  def serializePostcodeRestaurant(postcodeRestaurants: PostcodeRestaurants): JsObject =
+    Json.obj(
+      "postcode" -> postcodeRestaurants.postcode,
+      "fetch_time" -> LocalDateTime.now.toString,
+      "data" -> postcodeRestaurants.restaurants
+    )
+
+  val postcodeResponseSerializer: Sink[PostcodeRestaurants, Future[IOResult]] = {
+    val outputOpenOptions = Set(
+      StandardOpenOption.CREATE,
+      StandardOpenOption.WRITE
+    )
+    Flow[PostcodeRestaurants]
+      .map { serializePostcodeRestaurant }
+      .map { json => ByteString(Json.stringify(json) + '\n') }
+      .toMat(FileIO.toPath(outputPath, outputOpenOptions))(Keep.right)
+  }
 
   val postcodes: List[String] = PostcodeLoader.load()  // Load the list of postcodes to query
 
   // Define a source of restaurant arrays
   val postcodeResponses: Source[PostcodeRestaurants, NotUsed] =
     Source(postcodes.take(100))
-      // Query the API for a postcode
+  // Query the API for a postcode
       .mapAsync(8) { postcode =>
         YelpApi.fetchPostcode(postcode).map { response => (postcode -> response) }
       }
       .filter { case (postcode, response) => response.status == 200 }
-      // extract the restaurants in that postcode
+  // extract the restaurants in that postcode
       .map { case(postcode, response) =>
         val restaurants = YelpApi.parseSuccessfulResponse(postcode, response)
         PostcodeRestaurants(postcode, restaurants)
       }
 
-  postcodeResponses.runForeach {
-    case PostcodeRestaurants(postcode, restaurants) => println(restaurants)
-  }
+  val ioResultFuture = postcodeResponses.runWith(postcodeResponseSerializer)
 
-  Thread.sleep(10000) // give the stream time to materialize
+  val ioResult = Await.result(ioResultFuture, 60.seconds)
+  println(s"Written ${ioResult.count} bytes to $outputPath")
 
   // clean up
   ws.close()
